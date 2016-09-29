342ad59801a4b1faf741c87a3566e0dca6be6f57
Iain Mounsey-Smith <iain.mounseysmith@gmail.com>
2016-09-29 20:08:16 +1300

    test

--- GoogleWebToolkit/SuiteTest4GIT.java
+++ GoogleWebToolkit/SuiteTest4GIT.java
@@ -3,14 +3,14 @@ package com.selenium.GoogleWebToolkit;
 import com.selenium.PageObjects.refactorExampleOne.OriginalCodeRefactoredTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-
+import com.selenium.GoogleWebToolkit.*;
 /**
  * Created by Iain Mounsey-Smith on 29/09/2016.
  */
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({//we separate classes with a comma
-        OriginalCodeRefactoredTest.class
+        exampleGitBasedTests.class
 })
 public class SuiteTest4GIT {
 }
